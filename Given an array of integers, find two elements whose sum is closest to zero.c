45a4c588a8e33b858f2df13e4c300aeb50b92c6e
Praneesh Bhatt <praneeshbhatt071107@gmail.com>
2026-02-19 20:24:46 +0530

    Create Given an array of integers, find two elements whose sum is closest to zero.c

---
+++ Given an array of integers, find two elements whose sum is closest to zero.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Comparison function for qsort
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Sort the array
+    qsort(arr, n, sizeof(int), compare);
+    
+    int left = 0, right = n - 1;
+    int minSum = __INT_MAX__;
+    int ans1 = arr[left], ans2 = arr[right];
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        
+        if (abs(sum) < abs(minSum)) {
+            minSum = sum;
+            ans1 = arr[left];
+            ans2 = arr[right];
+        }
+        
+        // Move pointers
+        if (sum < 0)
+            left++;
+        else
+            right--;
+    }
+    
+    printf("%d %d\n", ans1, ans2);
+    return 0;
+}
